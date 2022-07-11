e477ddbf09214834be123453a311b3d36a4d63ee
Yslan Ramos <yslanramos.r@gmail.com>
2022-07-11 11:39:43 -0300

    feat(zsh) more plugins

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -4,14 +4,15 @@
   programs.zsh = {
     oh-my-zsh = {
       enable = true;
-      plugins = [ "git" ];
+      plugins = [ "git" "command-not-found" ];
     };
 
     initExtra = ''
       source ${pkgs.spaceship-prompt}/share/zsh/site-functions/prompt_spaceship_setup
     '';
-    
+
     enable = true;
     enableAutosuggestions = true;
+    syntaxHighlighting = true;
   };
 }
